5c16b167919b33f829dec99bd39f51151bdd8a40
badnc <b.dunic@hotmail.com>
2021-03-18 20:41:11 +0100

    Index.js

---
+++ ex04/src/index.js
@@ -0,0 +1,12 @@
+var monitorsListArray = ["Apple", "Peach", "Berry"];
+function myMonitorsFunction(arr) {
+
+    var newMonitorsList = [...arr];
+
+
+
+    
+}
+
+console.log(myMonitorsFunction(monitorsListArray));
+module.exports = myMonitorsFunction;
